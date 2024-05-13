b13bd5124e786fd401b9dd05cecf97091542a9c2
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-05-13 11:32:09 -0700

    init firestore for users

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import { useLoginStore } from "@/store";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useLoginStore } from "@/store";
-import { auth } from "@/firebase/firebaseConfig";
-import { useRouter } from "next/navigation";
+import { auth, db } from "@/firebase/firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
 
 const SignUp = () => {
   const setUser = useLoginStore((state) => state.setUser);
   const isLoggedIn = useLoginStore((state) => state.isLoggedIn);
   const setIsLoggedIn = useLoginStore((state) => state.setIsLoggedIn);
+  const usersRef = collection(db, "users"); // Reference to the 'users' collection in Firestore
   const router = useRouter();
 
   useEffect(() => {
@@ -33,6 +35,12 @@ const SignUp = () => {
     }));
   };
 
+  // Add user to Firestore database
+  const addUserToFirestore = async () => {
+    const docRef = addDoc(usersRef, {
+      email: userForm.email,
+    });
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -41,6 +49,7 @@ const SignUp = () => {
       .then((userCredential) => {
         // Signed up AND signed in per Firebase docs
         setUser(userCredential.user);
+        addUserToFirestore();
         setIsLoggedIn(true);
         console.log("User signed up and signed in.");
         router.replace("/");
@@ -53,10 +62,7 @@ const SignUp = () => {
   };
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className="flex flex-col m-4 p-4 h-screen"
-    >
+    <form onSubmit={onSubmit} className="flex flex-col m-4 p-4 h-screen">
       <span className="mt-4">Email</span>
       <input
         className="border border-gray-300 rounded-md p-2"
@@ -67,7 +73,7 @@ const SignUp = () => {
       />
       <span className="mt-4">Password</span>
       <input
-      name="password"
+        name="password"
         className="border border-gray-300 rounded-md p-2"
         type="password"
         onChange={onChange}
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -9,9 +10,11 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_DB_URL,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export default app;
